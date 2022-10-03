81abce2e2b0f301d84eff73fefc733677e86391c
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2022-10-03 11:19:22 +0530

    changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Col, Container, Row } from "react-bootstrap";
 import { FaFacebookF, FaGoogle, FaInstagram } from "react-icons/fa";
 import googleplaylogo from "../Assests/Images/googleplaylogo.png";
 import applestorelogo from "../Assests/Images/appstorelogo.png";
-import { MdLocationPin,MdStayCurrentPortrait,MdDrafts} from "react-icons/md";
+import { MdLocationPin, MdStayCurrentPortrait, MdDrafts } from "react-icons/md";
 function Footer() {
   return (
     <>
@@ -11,7 +12,7 @@ function Footer() {
         <div className="footer_logocontainer p-3">
           <h4 className="text-center  text-white">Known Jobz</h4>
         </div>
-          <hr className="m-0 bg-white "/> 
+        <hr className="m-0 bg-white " />
         <div className="Footer_maincontents pt-5">
           <Container>
             <Row lg={4} md={2} xs={1} className="px-xl-5 px-1 mx-0">
@@ -21,21 +22,37 @@ function Footer() {
                   className="text-white p-0 Quick_linklist"
                   style={{ listStyleType: "none" }}
                 >
-                  <li>Home</li>
-                  <li>jobs</li>
-                  <li>Aboutus</li>
-                  <li>Contact</li>
-                  <li>Regsiter</li>
-                  <li>Login</li>
+                  <li>
+                    <Link to="/" className="text-decoration-none text-white">Home</Link>
+                    </li>
+                  <li>
+                    <Link to="/Searchjob" className="text-decoration-none text-white">jobs</Link>
+                    </li>
+
+                  <li>
+                  <Link to="/Aboutus" className="text-decoration-none text-white">About-us</Link>
+
+                  </li>
+                  <li>
+                  <Link to="/Contact" className="text-decoration-none text-white">Contact-us</Link>
+
+                  </li>
+                  <li>
+                  <Link to="/Register" className="text-decoration-none text-white">Register</Link>
+                  </li>
+                  <li>
+                  <Link to="/SeekerLoginforms" className="text-decoration-none text-white">Login</Link>
+                   
+                  </li>
                 </ul>
               </Col>
               <Col>
                 <h5 className="text-white fw-bold pb-3">Get the App now</h5>
                 <div className="mb-3 ">
-                  <img src={googleplaylogo} className='w-50'/>
+                  <img src={googleplaylogo} className='w-50' />
                 </div>
                 <div>
-                  <img src={applestorelogo} className='w-50'/>
+                  <img src={applestorelogo} className='w-50' />
                 </div>
               </Col>
               <Col>
@@ -64,7 +81,7 @@ function Footer() {
                     <p className="pt-1 text-white">+91-33433423</p>
                   </div>
                   <div className="d-flex Contact_icons">
-                  <MdDrafts className="mt-1 fs-4 me-2" />
+                    <MdDrafts className="mt-1 fs-4 me-2" />
                     <p className="pt-1 text-white">Knownjobz@gmail.com</p>
                   </div>
                 </div>
